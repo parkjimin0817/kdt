10adb15de4be781b02158b7c92376f3259e97695
parkjimin0817 <jimin0817@naver.com>
2025-04-18 14:46:30 +0900

    no message

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,11 +1,83 @@
 import './App.css'
-import JavaScript from './components/JavaScript'
-function App() {
+import Hello from './components/Hello'
+import Heading from './components/Heading'
+import VideoList from './components/VideoList'
 
+const videoData = [{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/aWv3nD5gr7M/mqdefault_6s.webp?du=3000&sqp=CMmwh8AG&rs=AOn4CLA8vZoBraT3ifWBZ0c_jZEHkSWCPQ",
+  title : "난 이걸로 먹고살래",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "8.3만",
+  date : "2시간 전"
+},{
+  thumbnail : "https://i.ytimg.com/an_webp/juM_qadVY1E/mqdefault_6s.webp?du=3000&sqp=CLa4h8AG&rs=AOn4CLDfjDK86G1GfhXWx0R_1QDHYsGwDw",
+  title : "길러준 은혜도 모르고...",
+  logo : "https://yt3.googleusercontent.com/wYRkjS6E0mMZ-np2jNwjVaCNzQMpxs1VkdQ_p25oe0aaSj0awd7f9xRUcrwI6rVOQE7kjZQ6l4A=s160-c-k-c0x00ffffff-no-rj",
+  channelName: "빵빵이의 일상",
+  views : "5.8만",
+  date : "일주일 전"
+}]
+
+function App() {
 
   return (
     <>
-      <JavaScript/>
+      {/* <Heading type="h2"/>
+      <Heading/>
+      <Heading>
+        무엇을 도와드릴까요?
+      </Heading>
+      <Hello/> */}
+      <VideoList videos={videoData}/>
     </>
   )
 }
---
+++ 10_React/react-project/src/components/VideoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import VideoCard from './VideoCard'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`
+const VideoList = ({videos}) => {
+  return (
+    <Container>
+        {videos.map((v, index) => <VideoCard key={index} video={v}/>)}
+    </Container>
+  )
+}
+
+export default VideoList
