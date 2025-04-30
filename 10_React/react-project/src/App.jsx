18c6587bb200c714346191e6d77c8073c8bf3d4d
parkjimin0817 <jimin0817@naver.com>
2025-04-30 15:22:35 +0900

    no message

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ThemeProvider } from 'styled-components'
 import './App.css'
+import IconButtons from './components/IconButtons'
+import GlobalStyle from './GlobalStyle'
+import { useState } from 'react'
+import { darkTheme, lightTheme } from './themes'
+import ThemeBox from './components/ThemeBox'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isDark, setIsDark] = useState(false);
+  const toggleTheme = () => setIsDark(!isDark);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <IconButtons/> */}
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <ThemeBox onToggleTheme={toggleTheme}/>
+      </ThemeProvider>
     </>
   )
 }
