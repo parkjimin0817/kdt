79d75bba12ccda149b3b38d07f79dc461553f8b2
parkjimin0817 <jimin0817@naver.com>
2025-05-01 15:39:27 +0900

    no message

--- 10_React/react-project/src/store/useCounterStore.jsx
+++ 10_React/react-project/src/store/useCounterStore.jsx
@@ -1,14 +1,14 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 const useCounterStore = create((set, get) => ({
-    count: 0,
-    increase: () => set((state) => ({count : state.count + 1})), //set의 콜백으로 상태를 바로 업데이트 (불변성 보장)
-    decrease: () => set ({count : get().count - 1}), //get()으로 현재 상태값을 직접 읽어서 업데이트
-    reset: () => set ({count: 0}),
-}))
+  count: 0,
+  increase: () => set((state) => ({ count: state.count + 1 })), //set의 콜백으로 상태를 바로 업데이트 (불변성 보장)
+  decrease: () => set({ count: get().count - 1 }), //get()으로 현재 상태값을 직접 읽어서 업데이트
+  reset: () => set({ count: 0 }),
+}));
 
 //create(() => {
 //  return 초기값;
 //})
 
-export default useCounterStore
+export default useCounterStore;
