6a860a908cdaf9257d43ad242361de82ea33b3b7
parkjimin0817 <jimin0817@naver.com>
2025-05-01 09:40:44 +0900

    no message

--- 10_React/react-project/src/App.jsx
+++ 10_React/react-project/src/App.jsx
@@ -1,20 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import PostListPage from './pages/PostListPage'
+import "./App.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PostListPage from "./pages/PostListPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/posts' element={<PostListPage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/posts" element={<PostListPage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
